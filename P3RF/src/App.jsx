39e5a38b41bbf605440d880288ad3c62b4f9d430
90blin <shaak1996@gmail.com>
2025-09-22 17:56:57 -0300

    Fix project structure

---
+++ P3RF/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>🚀 Performator 3.0</h1>
+      <p>✅ Site funcionando!</p>
+    </div>
+  )
+}
+
+export default App
--- PERFORMATOR-3000/PERFORMATOR 3.0/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Layout from '../Layout.jsx'
-
-function App() {
-  return <Layout />
-}
-
-export default App
